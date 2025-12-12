e68dbd802662a1cf6c73b3c3a8f7e185d845ac92
aikawayakin01-star <aikawayakin01@gmail.com>
2025-12-12 10:19:21 +0800

    Add files via upload

---
+++ at.js
@@ -0,0 +1,26 @@
+pannellum.viewer('lalamo', {
+        type: 'equirectangular',
+         /*ANY PHOTO-> panorama:'HERE'*/
+        panorama: 'ikaw-ah.jpg',
+        autoLoad: true,
+        autoRotate: true,
+        autoRotateSpeed: 6,
+    });
+
+pannellum.viewer('aray-ko', {
+        type: 'equirectangular',
+        /*ANY PHOTO-> panorama:'HERE'*/
+        panorama: 'dalawa.jpg',
+        autoLoad: true,
+        autoRotate: true,
+        autoRotateSpeed: 6,
+    });
+
+pannellum.viewer('pakak', {
+    type: 'equirectangular',
+     /*ANY PHOTO-> panorama:'HERE'*/
+    panorama: 'tatlo.jpg',
+    autoLoad: true,
+    autoRotate: true,
+    autoRotateSpeed: 6,
+});
